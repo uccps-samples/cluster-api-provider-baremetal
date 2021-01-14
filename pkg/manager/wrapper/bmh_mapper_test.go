db59a46e98e8813ad20eb486b4b6761f3c10a122
Sandhya Dasu <sdasu@redhat.com>
2021-01-14 16:23:39 -0500

    Updates to handle changes to handle.MapObject and Reconcile()
    handle.MapObject was removed in controller-runtime v0.7.0 and has
    been replaced with client.Object.
    Reconcile() now takes the context as its 1st arguement. This is also
    required to start using controller-runtime v0.7.0.

--- pkg/manager/wrapper/bmh_mapper_test.go
+++ pkg/manager/wrapper/bmh_mapper_test.go
@@ -7,7 +7,7 @@ import (
 	machinev1beta1 "github.com/openshift/machine-api-operator/pkg/apis/machine/v1beta1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"sigs.k8s.io/controller-runtime/pkg/handler"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 func TestMap(t *testing.T) {
